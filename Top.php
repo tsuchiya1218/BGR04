ac55bb805b434bcb0bf53ef0e32710c3c3e403bf
20jy0204 <20jy0204@jec.ac.jp>
2022-01-25 12:20:03 +0900

    インデント

--- Top.php
+++ Top.php
@@ -9,25 +9,28 @@ $_SESSION["name"] = "$name";
 
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <title>Top</title>
 </head>
+
 <body>
-	<img name=logo src="./img/logo.jpg" alt="logo" width="300" height="130">
+    <img name=logo src="./img/logo.jpg" alt="logo" width="300" height="130">
 
     <h3 style="text-align:right">ユーザー名</h3>
     <p>エリアからご当地ラーメンを検索する</p>
 
     <?php
-echo"<p style=\"text-align:right\">";
-echo"{$_SESSION["name"]}でログイン中</br>";
+    echo "<p style=\"text-align:right\">";
+    echo "{$_SESSION["name"]}でログイン中</br>";
 
-echo "<button onclick=\"location.href='logout.php'\">ログアウト</button>";
-echo "</p>";
-?>
+    echo "<button onclick=\"location.href='logout.php'\">ログアウト</button>";
+    echo "</p>";
+    ?>
 
     <img name="Top_img" src="./img/日本列島.png" alt="日本列島" onClick="document.location='syousai.html'">
     <input type="button" value="好みの味を探す" onClick="document.location='kensaku.html'">
 </body>
+
 </html>

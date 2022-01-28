2fb5fd93ff6bf451209c782f91dd7eea5966a423
20jy0203 <20jy0203@jec.ac.jp>
2022-01-28 11:07:28 +0900

    いんでんとしましうた

--- login.php
+++ login.php
@@ -1,40 +1,43 @@
 <?php
 session_start();
-$login="";
-if(!empty($_SESSION["login"])){
-    $login=$_SESSION["login"];
+$login = "";
+if (!empty($_SESSION["login"])) {
+    $login = $_SESSION["login"];
 };
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
-<meta charset="UTF-8">
-<title>ログインページ</title>
+    <meta charset="UTF-8">
+    <title>ログインページ</title>
 </head>
+
 <body>
-<img name=logo src="./img/logo.jpg" alt="logo" width="300" height="130">
-<p>ログインページ</p>
+    <img name=logo src="./img/logo.jpg" alt="logo" width="300" height="130">
+    <p>ログインページ</p>
 
-<?php
+    <?php
 
-if($login=="true"){
+    if ($login == "true") {
 
-    echo"<p style=\"text-align:right\">";
-    echo"{$_SESSION["name"]}<br>";
+        echo "<p style=\"text-align:right\">";
+        echo "{$_SESSION["name"]}<br>";
 
-    echo "<button onclick=\"location.href='logout.php'\">ログアウト</button>";
-    echo "</p>";
-};
-?>
+        echo "<button onclick=\"location.href='logout.php'\">ログアウト</button>";
+        echo "</p>";
+    };
+    ?>
 
 
-<form method="post" action="Top.php">
-ユーザー名:<input type="text" name="name" style="width:200px"></br>
-パスワード:<input type="password" name="pass" style="width:200px">
-<p><input type="submit" value="ログイン"></p>
-</form>
-<a href="passwasure.php"> パスワードを忘れた方はこちら</a>
-<a href=""> 新規会員登録はこちら</a>
+    <form method="post" action="Top.php">
+        ユーザー名:<input type="text" name="name" style="width:200px"></br>
+        パスワード:<input type="password" name="pass" style="width:200px">
+        <p><input type="submit" value="ログイン"></p>
+    </form>
+    <a href="passwasure.php"> パスワードを忘れた方はこちら</a>
+    <a href="touroku.php"> 新規会員登録はこちら</a>
 </body>
+
 </html>

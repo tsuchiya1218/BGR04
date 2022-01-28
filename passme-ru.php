92d2d834ce357a33e912aad51cf9b580819fa516
20jy0215 <20jy0215@jec.ac.jp>
2022-01-28 10:53:24 +0900

    パスワード変更メールphp作成

---
+++ passme-ru.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>メール送信完了</title>
+</head>
+<body>
+    <img name=logo src="./img/logo.jpg" alt="logo" width="300" height="130">
+
+    <h2>メール送信完了しました</h2>
+
+    <input type="button" onclick="location.href='login.php'" value="TOPページに戻る" />
+</body>
+</html>

ab7a5d47601b791c57018817706bfc6efdd1b15a
20jy0215 <20jy0215@jec.ac.jp>
2022-02-01 12:32:53 +0900

    好み検索

--- konomikensaku.php
+++ konomikensaku.php
@@ -1,5 +1,41 @@
 <?php
 session_start();
+
+try {
+	$server_name = "10.42.129.3";	// サーバ名
+	$db_name = "20jy0204";	// データベース名(自分の学籍番号を入力)
+
+	$user_name = "20jy0204";	// ユーザ名(自分の学籍番号を入力)
+	$user_pass = "20jy0204";	// パスワード(自分の学籍番号を入力)
+
+	// データソース名設定
+	$dsn = "sqlsrv:server=$server_name;database=$db_name";
+
+	// PDOオブジェクトのインスタンス作成
+	$pdo = new PDO($dsn, $user_name, $user_pass);
+
+	// PDOオブジェクトの属性の指定
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+	print "接続エラー!: " . $e->getMessage();
+	exit();
+}
+
+$sql = "SELECT * FROM goods WHERE ";
+
+try {
+	// SQL 文を準備
+	$stmt = $pdo->prepare($sql);
+	// SQL 文を実行
+	$stmt->execute(array($g_code));
+	// 実行結果をまとめて取り出し(カラム名で添字を付けた配列)
+	$array = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	$stmt = null;
+	$pdo = null;
+} catch (PDOException $e) {
+	print "SQL 実行エラー!: " . $e->getMessage();
+	exit();
+}
 ?>
 
 <!DOCTYPE html>
@@ -22,31 +58,24 @@ session_start();
     ?>
 
     <table border="1">
-        <tr onclick="location.href='syousai.php'">
-            <td><img src="ramen.jpg" alt="俺のミルクラーメン"></td>
-            <td>
-                俺のミルクラーメン
-            </td>
-            <td>
-                自家製ミルクで濃厚なラーメンをつくりあげました
-
-            </td>
-            <td>
-                &yen;300-
-            </td>
-        </tr>
-        <tr onclick="location.href='syousai.php'">
-            <td><img src="ramen.jpg" alt="TOKYO都民だと思ってましたラーメン"></td>
-            <td>
-                TOKYO都民だと思ってましたラーメン
-            </td>
-            <td>
-                IT`s　SEIRAラーメン
-            </td>
-            <td>
-                &yen;480-
-            </td>
-        </tr>
+    <tr>
+        <th>名前</th>
+        <th>イメージ</th>
+        <th>詳細</th>
+        <th>値段</th>
+        <th>アレルギー</th>
+    </tr>
+    <?php
+foreach($array as $value){
+    echo "<tr>\n";
+    echo "<td>{$value["g_name"]}</td>\n";
+    echo "<td>{$value["g_image"]}</td>\n";
+    echo "<td>{$value["g_detail"]}</td>\n";
+    echo "<td>{$value["price"]}</td>\n";
+    echo "<td>{$value["allergen"]}</td>\n";
+    echo "</tr>\n";
+}
+?>
     </table>
     <input type="button" onclick="location.href='kensaku.php'" value="戻る" />
 </body>

b3db93e29a0832a7ea40cd9dc2e898c22960120f
20jy0215 <20jy0215@jec.ac.jp>
2022-02-04 12:32:02 +0900

    sql追加(途中)

--- tyumon_k.php
+++ tyumon_k.php
@@ -1,5 +1,54 @@
 <?php
 session_start();
+
+try {
+	$server_name = "10.42.129.3";	// サーバ名
+	$db_name = "20jy0204";	// データベース名(自分の学籍番号を入力)
+
+	$user_name = "20jy0204";	// ユーザ名(自分の学籍番号を入力)
+	$user_pass = "20jy0204";	// パスワード(自分の学籍番号を入力)
+
+	// データソース名設定
+	$dsn = "sqlsrv:server=$server_name;database=$db_name";
+
+	// PDOオブジェクトのインスタンス作成
+	$pdo = new PDO($dsn, $user_name, $user_pass);
+
+	// PDOオブジェクトの属性の指定
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+	print "接続エラー!: " . $e->getMessage();
+	exit();
+}
+
+$sql = "SELECT * FROM cart inner join customers on costomers.c_code = cart.c_code 
+
+
+
+
+inner join goods on goods.g_code = cart.g_code";
+
+try {
+	// SQL 文を準備
+	$stmt = $pdo->prepare($sql);
+	// SQL 文を実行
+	$stmt->execute();
+	// 実行結果をまとめて取り出し(カラム名で添字を付けた配列)
+	$array = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	$stmt = null;
+	$pdo = null;
+} catch (PDOException $e) {
+	print "SQL 実行エラー!: " . $e->getMessage();
+	exit();
+}
+foreach ($array as $value) { 
+    //データベースから顧客情報取得
+    $c_name = $value["c_name"];
+    $c_address1 = $value["c_address1"];
+    $c_address2 = $value["c_address2"];
+}
+
+echo $_POST["card"];
 ?>
 
 <!DOCTYPE html>
@@ -22,11 +71,35 @@ session_start();
     echo "</p>";
     ?>
 
-    <p>住所：東京都新宿区○○１－１－１</p>
-    <p>カード会社：あめっくす</p>
-    <img src="./img/ramen.jpg" alt="">
-    <p>商品：札幌ラーメン</p>
-    <p>価格：&yen;480-</p><br>
+    <p>住所：<?=$c_address1?>.<?=$c_address1?></p>
+    <p>カード会社：<?=$card?></p>
+    <table border="2">
+        <tr>
+            <td>商品画像</td>
+            <td>商品名</td>
+            <td>数量</td>
+            <td>小計</td>
+        </tr>
+        <?php
+        $totalprice = 0;
+        foreach($array as $value){
+            echo "<tr>";
+                echo "<td><img src='img/{$value["g_image"]}' alt='g_image' ></td>";
+                echo "<td>{$value["g_name"]}<br>{$value["shopname"]}</td>";
+                echo "<td>{$value["qty"]}個</td>";
+                $syoukei = $value["qty"] * $value["price"];
+                echo "<td>".$syoukei."円</td>";
+                $totalprice = $totalprice + $syoukei;
+            echo "</tr>";
+        }
+        ?>
+            <tr>
+                <td></td>
+                <td></td>
+                <td>合計金額</td>
+                <td><?= $totalprice ?>円</td>
+            </tr>
+            </table>
 
     <p>以上の内容で注文を確定してもよろしいですか？</p>
 

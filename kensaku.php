c01754e82c59d9686e14c2b476efa31c226a59e6
20jy0204 <20jy0204@jec.ac.jp>
2022-02-18 10:17:01 +0900

    名前

--- kensaku.php
+++ kensaku.php
@@ -39,7 +39,7 @@ session_start();
 
     <h3 style="text-align:right">ユーザー名</h3>
     <h2 style="text-align:center">あなたの好みを探します</h2>
-    <form method="POST" action="konomikensaku.php">
+    <form method="POST" action="list.php.php">
         <div>
             <label><input type="radio" name="type" value="醤油" required>醤油</label>
             <label><input type="radio" name="type" value="味噌">味噌</label>
